c6651dfebb0a1bb8493d4e31461d5052f4507467
Nicolas Jacquemin <Nicolas.jcqm@gmail.com>
2018-02-09 09:52:33 +0100

    inscription

--- src/main/java/org/lpro/entity/Utilisateur.java
+++ src/main/java/org/lpro/entity/Utilisateur.java
@@ -6,10 +6,13 @@
 package org.lpro.entity;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -26,10 +29,17 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Utilisateur implements Serializable{
     
     @Id
-    @GeneratedValue
     private String id;
+    
+    @NotNull
     private String username;
+    
+    @NotNull
+    @Column(unique = true)
+    //@Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?.)+[a-z0-9](?:[a-z0-9-][a-z0-9])?|[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")
     private String mail;
+    
+    @NotNull
     private String password;
     
     public Utilisateur(){
